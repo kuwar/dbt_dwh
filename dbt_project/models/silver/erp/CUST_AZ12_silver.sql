2b4af7b18a467973c05f49592e17ccb9925f59f4
Shaurave <kuwarsaurav21@gmail.com>
2025-04-22 23:14:13 +0200

    feat: silver - transformed location information

--- dbt_project/models/silver/erp/CUST_AZ12_silver.sql
+++ dbt_project/models/silver/erp/CUST_AZ12_silver.sql
@@ -5,17 +5,18 @@
 }}
 
 SELECT
-			CASE
-				WHEN "CID" LIKE 'NAS%' THEN SUBSTRING("CID", 4, LENGTH("CID")) -- Remove 'NAS' prefix if present
-				ELSE "CID"
-			END AS cid, 
-			CASE
-				WHEN "BDATE" > CURRENT_DATE THEN NULL
-				ELSE "BDATE"
-			END AS bdate, -- Set future birthdates to NULL
-			CASE
-				WHEN UPPER(TRIM("GEN")) IN ('F', 'FEMALE') THEN 'Female'
-				WHEN UPPER(TRIM("GEN")) IN ('M', 'MALE') THEN 'Male'
-				ELSE 'n/a'
-			END AS gen -- Normalize gender values and handle unknown cases
-		FROM {{ ref("CUST_AZ12_bronze") }}
+	CASE
+		WHEN "CID" LIKE 'NAS%' THEN SUBSTRING("CID", 4, LENGTH("CID")) -- Remove 'NAS' prefix if present
+		ELSE "CID"
+	END AS cid, 
+	CASE
+		WHEN "BDATE" > CURRENT_DATE THEN NULL
+		ELSE "BDATE"
+	END AS bdate, -- Set future birthdates to NULL
+	CASE
+		WHEN UPPER(TRIM("GEN")) IN ('F', 'FEMALE') THEN 'Female'
+		WHEN UPPER(TRIM("GEN")) IN ('M', 'MALE') THEN 'Male'
+		ELSE 'n/a'
+	END AS gen, -- Normalize gender values and handle unknown cases
+	CURRENT_TIMESTAMP as dwh_create_date
+FROM {{ ref("CUST_AZ12_bronze") }}
---
+++ dbt_project/models/silver/erp/LOC_A101_silver.sql
@@ -0,0 +1,16 @@
+{{
+  config(
+    tags=['silver', 'customer', 'location', 'country', 'geography']
+  )
+}}
+
+SELECT
+  REPLACE("CID", '-', '') AS cid, 
+  CASE
+    WHEN TRIM("CNTRY") = 'DE' THEN 'Germany'
+    WHEN TRIM("CNTRY") IN ('US', 'USA') THEN 'United States'
+    WHEN TRIM("CNTRY") = '' OR "CNTRY" IS NULL THEN 'n/a'
+    ELSE TRIM("CNTRY")
+  END AS cntry, -- Normalize and Handle missing or blank country codes
+  CURRENT_TIMESTAMP as dwh_create_date
+FROM {{ ref("LOC_A101_bronze") }}
