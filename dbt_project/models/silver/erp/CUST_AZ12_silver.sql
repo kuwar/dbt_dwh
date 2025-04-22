644e774f1295e9213ed70ebb1583ecc5ba455ccd
Shaurave <kuwarsaurav21@gmail.com>
2025-04-22 22:40:18 +0200

    feat: silver - customer demography transformed

---
+++ dbt_project/models/silver/erp/CUST_AZ12_silver.sql
@@ -0,0 +1,21 @@
+{{
+  config(
+    tags=['silver', 'demography', 'customer', 'dateofbirth', 'birth', 'gender']
+  )
+}}
+
+SELECT
+			CASE
+				WHEN "CID" LIKE 'NAS%' THEN SUBSTRING("CID", 4, LENGTH("CID")) -- Remove 'NAS' prefix if present
+				ELSE "CID"
+			END AS cid, 
+			CASE
+				WHEN "BDATE" > CURRENT_DATE THEN NULL
+				ELSE "BDATE"
+			END AS bdate, -- Set future birthdates to NULL
+			CASE
+				WHEN UPPER(TRIM("GEN")) IN ('F', 'FEMALE') THEN 'Female'
+				WHEN UPPER(TRIM("GEN")) IN ('M', 'MALE') THEN 'Male'
+				ELSE 'n/a'
+			END AS gen -- Normalize gender values and handle unknown cases
+		FROM {{ ref("CUST_AZ12_bronze") }}
