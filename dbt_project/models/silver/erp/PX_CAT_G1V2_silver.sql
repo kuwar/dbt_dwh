ce4fb0c7838c6a2bd175913d70ebb49ed2efa6f4
Shaurave <kuwarsaurav21@gmail.com>
2025-04-22 23:28:47 +0200

    feat: silver - product category transformed

---
+++ dbt_project/models/silver/erp/PX_CAT_G1V2_silver.sql
@@ -0,0 +1,13 @@
+{{
+  config(
+    tags=['silver', 'product', 'category', 'subcategory', 'maintenance']
+  )
+}}
+
+SELECT 
+  "ID" AS id,
+  "CAT" AS cat,
+  "SUBCAT" AS subcat,
+  "MAINTENANCE" AS maintenance,
+  CURRENT_TIMESTAMP AS dwh_create_date
+FROM {{ ref("PX_CAT_G1V2_bronze") }}
